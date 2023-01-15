2e2dd2a3da6bb94562ae9798a67e509510b9ac6d
Arar <chanpedrias@gmail.com>
2023-01-15 12:03:10 +0800

    Added modulus and exponent function

---
+++ openpyxl_src/read_excel.py
@@ -0,0 +1,27 @@
+"""
+read_excel.py
+    Capabilities:
+
+    Limitations:
+
+    Prerequisite:
+        pip install openpyxl
+"""
+
+import openpyxl
+workbook = openpyxl.load_workbook('/home/renzo/Documents/git/excel_sample.xlsx')
+
+# Print the name of the sheets in excel file
+# sheets = workbook.sheetnames
+# print(sheets)
+
+# Print the active title(tab_name) in the excel
+# print(workbook.active.title)
+
+sheet_1 = workbook['Info']
+# Store the value of any cell inside Info tab
+value = sheet_1['C4'].value
+print(f'Name: {value}')
+
+
+
--- quick_math.py
+++ quick_math.py
@@ -1,41 +1,52 @@
 """
-    Improvement:
-        Add modulo
+    Capabilities:
+        Calculate muliple numbers up to 99 using the same operators
+    Limitations:
+        Can't use multiple operators at the same time
 """
 
-import math
+# import math
 import sys
 
 if len(sys.argv) < 4:
-    print("Usage: quick_math.py <add|mul|div|sub|avg> <num01> <num02> [num03..num99]")
+    print("Usage: quick_math.py <add|sub|mul|div|avg|pow> <num01> <num02> [num03..num99]")
     exit(1)
 
-w_addition = {"+", "add", "addition", "plus"}
-w_subtraction = {"-", "sub", "subtraction", "minus"}
-w_multiply = {"*", "x", "mul", "multiply"}
-w_divide = {"/", "div", "divide"}
+addition = {"+", "add", "addition", "plus"}
+subtraction = {"-", "sub", "subtract", "subtraction", "minus"}
+multiply = {"*", "x", "mul", "multiply"}
+divide = {"/", "div", "divide"}
+modulo = {"%", "avg", "average", "mod", "modulus", "modulo" }
+exponent = {"**", "pow", "exponent"}
 
 for counter in range(2, len(sys.argv)):
     if not sys.argv[counter].isdigit():
-        print("Usage: quick_math.py <add|mul|div|sub|avg> <num01> <num02> [num03..num99]")
+        print("Usage: quick_math.py <add|sub|mul|div|avg|pow> <num01> <num02> [num03..num99]")
         exit(1)
 
 result = float(sys.argv[2])
 
-if sys.argv[1] in w_addition:
+if sys.argv[1] in addition:
     for counter in range(3, len(sys.argv)):
         result = result + float(sys.argv[counter])
-elif sys.argv[1] in w_subtraction:
+elif sys.argv[1] in subtraction:
     for counter in range(3, len(sys.argv)):
         result = result - float(sys.argv[counter])
-elif sys.argv[1] in w_multiply:
+elif sys.argv[1] in multiply:
     for counter in range(3, len(sys.argv)):
         result = result * float(sys.argv[counter])
-elif sys.argv[1] in w_divide:
+elif sys.argv[1] in divide:
     for counter in range(3, len(sys.argv)):
         result = result / float(sys.argv[counter])
+elif sys.argv[1] in modulo:
+    for counter in range(3, len(sys.argv)):
+        result = result % float(sys.argv[counter])
+elif sys.argv[1] in exponent:
+    for counter in range(3, len(sys.argv)):
+        result = result ** float(sys.argv[counter])
 else:
-    print("Usage: quickmath.py <add|mul|div|sub|avg> <num01> <num02> [num03..num99]")
+    print("Usage: quickmath.py <add|mul|div|sub|avg|pow> <num01> <num02> [num03..num99]")
     exit(1)
 
-print(str(result))
+# print(str(result))
+print(f'\nResult: {result:,}\n')
