6e05cb9ba61a906c1f1c14fa04048ba99f27c518
renzo <chanpedrias@gmail.com>
2023-01-19 04:36:51 +0800

    Initial commit

---
+++ time_converter.py
@@ -0,0 +1,6 @@
+from datetime import timedelta
+
+print('Second: ', timedelta(seconds = 1))
+print('Minute: ', timedelta(seconds = 60))
+print('Hour:   ', timedelta(seconds = 60*60))
+print('Day:    ', timedelta(seconds= 24*60*60))
