7b3fbdd9071320268e2e787fe7781582177c9a90
arar <chanpedrias@gmail.com>
2023-01-28 02:21:55 +0800

    Initial commit

---
+++ pick_random.py
@@ -0,0 +1,11 @@
+import random
+
+# Pick random number
+random.random()  # returns a float between 0 to 1
+random.randint(1, 6)  # returns an int between 1 to 6
+
+# Pick random person
+members = ['John', 'Bob', 'Mary']
+leader = random.choice(members)  # randomly picks an item
+
+print(leader)
