c59bb222767cba615cae8452dcd0d23be7ebcef2
arar <chanpedrias@gmail.com>
2023-01-28 16:05:07 +0800

    Minor changes

--- array_sample.py
+++ array_sample.py
@@ -1,11 +1,18 @@
 import array as arr
 
-numbers = arr.array('i', [1, 2, 3, 5, 7, 10])
+numbers = arr.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,])
+
+# Print original array
+print(f'\nOriginal: {numbers}')
 
 # Changing first element
-numbers[0] = 0
-print(numbers)     # Output: array('i', [0, 2, 3, 5, 7, 10])
+numbers[0] = -1
+print(f'Change the first element: {numbers}')
 
-# Changing 3rd to 5th element
-numbers[2:5] = arr.array('i', [4, 6, 8])
-print(numbers)     # Output: array('i', [0, 2, 4, 6, 8, 10])
+# Get the last element
+numbers[-1] = 20
+print(f'Change the last element: {numbers}')
+
+# Change 3rd to 5th element
+numbers[2:5] = arr.array('i', [100, 101, 102])
+print(f'Changing 3rd to 5th element: {numbers}')
